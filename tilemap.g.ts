fe15d8a0c48ebb588e274e09feb1d070740fbfbf
h31lo0000 <107245819+h31lo0000@users.noreply.github.com>
2022-06-10 09:55:45 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`100010000209090909090909090909090909090108030303030303030303030303030306080303030a0303030a03030a0303030608030303030303030303030303030306080303030303030a0303030303030a060803030a03030303030a030303030a060803030303030303030303030303030608030303030a0303030303030303030608030303030303030303030303030306080303030303030a0303030303030306080909090b0103030303030303030306080d03030a0603030303030303030306080303030306030303030303030303060803030303060c0303030303030a03060803030303060303030303030303030604070707070707070707070707070705`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 . 2 . . . . . . . . . 2 
+2 . . . . 2 . . . . . . . . . 2 
+2 . . . . 2 . . . . . . . . . 2 
+2 . . . . 2 . . . . . . . . . 2 
+2 . . . . 2 . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.floorLight0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight4,sprites.dungeon.stairEast,sprites.dungeon.floorLight1,sprites.dungeon.chestClosed], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
